bceb33ee1b89f3ba6a6bba50d9f32c21f6a5b3b0
acifre <78503928+acifre@users.noreply.github.com>
2023-01-24 16:04:52 -0600

    Add flagTapped, askQuestion, resetGame functions

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var showAlert = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var numberOfTurns = 0
     
     
-    var countries = ["Estonia", "US", "Spain", "Ireland", "France", "Russia", "Italy", "UK", "Nigeria", "Monaco", "Germany", "Poland"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "US", "Spain", "Ireland", "France", "Russia", "Italy", "UK", "Nigeria", "Monaco", "Germany", "Poland"]
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var countryToGuess: String? {
         return countries.randomElement() ?? "US"
@@ -21,44 +25,74 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-
             
-                ZStack {
-                    VStack(spacing: 0) {
-                        Color.red
-                        Color.blue
-                    }
-
+            ZStack {
+                
+                LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 30) {
+                    
                     VStack {
-                        Text("What flag is for")
-                            .foregroundColor(.secondary)
-                            .padding(10)
-                            .background(.ultraThinMaterial)
-                        Text("United States")
-                            .foregroundColor(.secondary)
-                            .padding(10)
-                            .background(.ultraThinMaterial)
-                        Button {
-                            // some action
-                        } label: {
-                            Image("France")
-                        }
-                        Button {
-                            // some action
-                        } label: {
-                            Image("Italy")
-                        }
-                        Button {
-                            // some action
-                        } label: {
-                            Image("US")
-                        }
-
+                        Text("Tap the flag of")
+                            .foregroundColor(.white)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundColor(.white)
+                            .font(.largeTitle.weight(.semibold))
+                        
+                        
+                        
+                        
                     }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
+                    
                 }
                 .ignoresSafeArea()
                 .navigationTitle("GuessTheFlag")
+                .alert("\(scoreTitle)", isPresented: $showAlert) {
+                    Button(numberOfTurns < 5 ? "Continue" : "Reset Game", action: numberOfTurns < 5 ? askQuestion : resetGame)
+                } message: {
+                    
+                    if numberOfTurns < 5 {
+                        Text("Your score is \(score).\n\(score) / \(numberOfTurns) correct!")
+                    } else {
+                        Text("Game Over\nYou got \(score) / \(numberOfTurns) correct!")
+                    }
+                    
+                }
+            }
         }
+    }
+    
+    func flagTapped(_ number: Int) {
+        scoreTitle = number == correctAnswer ? "Correct" : "Wrong"
+        if number == correctAnswer {
+            score += 1
+        }
+        
+        numberOfTurns += 1
+        showAlert = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetGame() {
+        askQuestion()
+        numberOfTurns = 0
+        score = 0
     }
 }
 
