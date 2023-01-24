66fdc22d8c7fc40f205fe288673f0cfe76358d63
acifre <78503928+acifre@users.noreply.github.com>
2023-01-24 08:16:54 -0600

    Add UI skeleton and add countries and correctAnswer variables

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showAlert = false
+    
+    
+    var countries = ["Estonia", "US", "Spain", "Ireland", "France", "Russia", "Italy", "UK", "Nigeria", "Monaco", "Germany", "Poland"]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var countryToGuess: String? {
+        return countries.randomElement() ?? "US"
+    }
+    
     var body: some View {
         NavigationView {
 
@@ -18,10 +29,32 @@ struct ContentView: View {
                         Color.blue
                     }
 
-                    Text("Your content")
-                        .foregroundColor(.secondary)
-                        .padding(50)
-                        .background(.ultraThinMaterial)
+                    VStack {
+                        Text("What flag is for")
+                            .foregroundColor(.secondary)
+                            .padding(10)
+                            .background(.ultraThinMaterial)
+                        Text("United States")
+                            .foregroundColor(.secondary)
+                            .padding(10)
+                            .background(.ultraThinMaterial)
+                        Button {
+                            // some action
+                        } label: {
+                            Image("France")
+                        }
+                        Button {
+                            // some action
+                        } label: {
+                            Image("Italy")
+                        }
+                        Button {
+                            // some action
+                        } label: {
+                            Image("US")
+                        }
+
+                    }
                 }
                 .ignoresSafeArea()
                 .navigationTitle("GuessTheFlag")
