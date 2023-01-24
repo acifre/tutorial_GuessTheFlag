0f3b35215c62cce01af05f4a45270b84d5135576
acifre <78503928+acifre@users.noreply.github.com>
2023-01-24 16:36:49 -0600

    Fix UI and add correctMessages array

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,13 +11,17 @@ struct ContentView: View {
     
     @State private var showAlert = false
     @State private var scoreTitle = ""
+    @State private var alertMessage = ""
     @State private var score = 0
     @State private var numberOfTurns = 0
+    
     
     
     @State private var countries = ["Estonia", "US", "Spain", "Ireland", "France", "Russia", "Italy", "UK", "Nigeria", "Monaco", "Germany", "Poland"]
     
     @State private var correctAnswer = Int.random(in: 0...2)
+    
+    let correctMessages = ["Great job!", "You're a pro!", "You know your stuff...", "Keep it up!"]
     
     var countryToGuess: String? {
         return countries.randomElement() ?? "US"
@@ -28,54 +32,76 @@ struct ContentView: View {
             
             ZStack {
                 
-                LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-                    .ignoresSafeArea()
-                
-                VStack(spacing: 30) {
-                    
-                    VStack {
-                        Text("Tap the flag of")
-                            .foregroundColor(.white)
-                            .font(.subheadline.weight(.heavy))
-                        Text(countries[correctAnswer])
-                            .foregroundColor(.white)
-                            .font(.largeTitle.weight(.semibold))
-                        
-                        
-                        
-                        
-                    }
-                    ForEach(0..<3) { number in
-                        Button {
-                            flagTapped(number)
-                        } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
-                        }
-                    }
-                    
-                }
+                RadialGradient(stops: [
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+                ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-                .navigationTitle("GuessTheFlag")
+                
+                VStack {
+                    Spacer()
+                    Text("Guess the Flag")
+                            .font(.largeTitle.bold())
+                            .foregroundColor(.white)
+                    Spacer()
+                    VStack(spacing: 15) {
+                        
+                        VStack {
+                            Text("Tap the flag of")
+                                .foregroundStyle(.secondary)
+                                .font(.subheadline.weight(.heavy))
+                                
+                            Text(countries[correctAnswer])
+                                .foregroundColor(.white)
+                                .font(.largeTitle.weight(.semibold))
+                            
+                        }
+                        
+                        ForEach(0..<3) { number in
+                            Button {
+                                flagTapped(number)
+                            } label: {
+                                Image(countries[number])
+                                    .renderingMode(.original)
+                                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                                    .shadow(radius: 5)
+                            }
+                        }
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                    .background(.regularMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    Spacer()
+                    Spacer()
+                    Text("Score: \(score)")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+                    Spacer()
+
+
+                }
+                .padding()
                 .alert("\(scoreTitle)", isPresented: $showAlert) {
-                    Button(numberOfTurns < 5 ? "Continue" : "Reset Game", action: numberOfTurns < 5 ? askQuestion : resetGame)
+                    Button(numberOfTurns < 8 ? "Continue" : "Reset Game", action: numberOfTurns < 8 ? askQuestion : resetGame)
                 } message: {
                     
-                    if numberOfTurns < 5 {
-                        Text("Your score is \(score).\n\(score) / \(numberOfTurns) correct!")
+                    if numberOfTurns < 8 {
+                        Text("\(alertMessage)\n\(score) / \(numberOfTurns) correct!")
                     } else {
                         Text("Game Over\nYou got \(score) / \(numberOfTurns) correct!")
                     }
                     
                 }
+                
             }
         }
     }
     
     func flagTapped(_ number: Int) {
         scoreTitle = number == correctAnswer ? "Correct" : "Wrong"
+        alertMessage = number == correctAnswer ? correctMessages.randomElement()! : "That's the flag of \(countries[number])."
         if number == correctAnswer {
             score += 1
         }
